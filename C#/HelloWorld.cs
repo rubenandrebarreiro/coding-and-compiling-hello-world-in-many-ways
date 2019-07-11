ad424f2b950b875631c76f52539350ad631af69b
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-11 22:22:56 +0100

    Hello World in C#
    Hello World in C#

---
+++ C#/HelloWorld.cs
@@ -0,0 +1,19 @@
+/**
+*
+* Hello World in Many Programming Languages!!!
+*
+* Authors:
+* @author Ruben Andre Barreiro
+*
+*/
+using System;
+
+namespace HelloWorld {
+    class Hello {
+        static void Main() {
+			// Prints "Hello World!!!" to the terminal window
+            Console.WriteLine("Hello World!!!");
+			Console.ReadKey();
+        }
+    }
+}
