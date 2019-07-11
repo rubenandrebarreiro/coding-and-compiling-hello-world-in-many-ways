0ec46543836e95c2af959700cb540e647d7987c8
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-11 21:50:27 +0100

    Hello World in Java
    Hello World in Java

---
+++ Java/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+*
+* Hello World in Many Programming Languages
+*
+* Authors:
+* @author Ruben Andre Barreiro
+*
+*/
+
+public class HelloWorld {
+    
+	public static void main(String[] args) {
+        
+		// Prints "Hello World!!!" to the terminal window
+        System.out.println("Hello World!!!");
+    }
+	
+}
