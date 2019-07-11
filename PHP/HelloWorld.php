06105d3708d966db20f0635719050cad6c079108
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-11 22:54:48 +0100

    Hello World in PHP
    Hello World in PHP

---
+++ PHP/HelloWorld.php
@@ -0,0 +1,16 @@
+<!-- 
+* Hello World in Many Programming Languages!!!
+*
+* Authors:
+* @author Ruben Andre Barreiro
+-->
+
+<html>
+ <head>
+  <title>Hello World!!!</title>
+ </head>
+ <body>
+ <!-- Prints "Hello World!!!" to the Web Browser window -->
+ <?php echo '<p>Hello World!!!</p>'; ?> 
+ </body>
+</html>
