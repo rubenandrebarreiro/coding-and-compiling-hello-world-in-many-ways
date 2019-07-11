62126cfe3afc41258081109ecc10539fdc6b0b0a
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-11 22:40:52 +0100

    Hello World in Scala
    Hello World in Scala

---
+++ Scala/HelloWorld.scala
@@ -0,0 +1,14 @@
+/**
+*
+* Hello World in Many Programming Languages!!!
+*
+* Authors:
+* @author Ruben Andre Barreiro
+*
+*/
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!!!")
+  }
+}
