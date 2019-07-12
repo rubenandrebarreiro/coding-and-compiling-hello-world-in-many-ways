1af963271e56504e8b4d758ec3cd4bc95360db17
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-12 14:54:28 +0100

    Hello World in C
    Hello World in C

---
+++ C/HelloWorld.c
@@ -0,0 +1,18 @@
+/**
+*
+* Hello World in Many Programming Languages!!!
+*
+* Authors:
+* @author Ruben Andre Barreiro
+*
+*/
+
+#include <stdio.h>
+
+int main() {
+    
+	// Prints "Hello World!!!" to the terminal window
+    printf("Hello World!!!");
+    
+    return 0;
+}
