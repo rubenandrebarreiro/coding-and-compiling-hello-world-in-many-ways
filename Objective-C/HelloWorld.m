61ca82f621f538d23b73584d316d54cb6035a5fa
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-12 15:59:46 +0100

    Hello World in Objective-C
    Hello World in Objective-C

---
+++ Objective-C/HelloWorld.m
@@ -0,0 +1,21 @@
+/**
+*
+* Hello World in Many Programming Languages!!!
+*
+* Authors:
+* @author Ruben Andre Barreiro
+*
+*/
+
+#import <Foundation/Foundation.h>
+ 
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    
+    // Prints "Hello World!!!" to the terminal window
+    NSLog(@"***Hello World!***");//This will output Hello World!
+ 
+    [pool release];
+    
+    return 0;
+}
