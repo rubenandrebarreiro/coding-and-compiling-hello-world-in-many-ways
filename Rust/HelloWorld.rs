2ab70877af4d9820177de1c07b0df53a4b9dadf4
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-12 09:08:11 +0100

    Hello World in Rust
    Hello World in Rust

---
+++ Rust/HelloWorld.rs
@@ -0,0 +1,8 @@
+// Hello World in Many Programming Languages!!!
+// Authors:
+// - Ruben Andre Barreiro
+
+fn main() {   
+    // Prints "Hello World!!!" to the terminal window
+    println!("Hello World!!!");
+}
