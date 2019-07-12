0191b4a965b1617e049cde2ebb5c12d2b158acf8
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-12 17:37:32 +0100

    Hello World in Haxe
    Hello World in Haxe

---
+++ Haxe/HelloWorld.hx
@@ -0,0 +1,16 @@
+/**
+*
+* Hello World in Many Programming Languages!!!
+*
+* Authors:
+* @author Ruben Andre Barreiro
+*
+*/
+
+class HelloWorld {
+  static public function main():Void {
+   
+    // Prints "Hello World!!!" to the terminal window
+    trace("Hello World!!!\n");
+  }
+}
