6d9397c1b08c28d947818a561c59022a75d88b7d
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-12 14:27:02 +0100

    Hello World in Erlang
    Hello World in Erlang

--- Erlang/HelloWorld.erl
+++ Erlang/HelloWorld.erl
@@ -1,4 +1,9 @@
--module(hello).
+% Hello World In Many Programming Languages!!!
+% Authors:
+% @author Ruben Andre Barreiro
+
+-module(helloWorld).
 -export([hello_world/0]).
 
+% Prints "Hello World!!!" to the terminal window
 hello_world() -> io:fwrite("Hello World!!!\n").
