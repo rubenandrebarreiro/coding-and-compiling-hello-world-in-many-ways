61bf1c4a2168be95928d70360d88241d2b25bb47
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-12 16:43:43 +0100

    Hello World in R
    Hello World in R

--- R/HelloWorld.R
+++ R/HelloWorld.R
@@ -1,1 +1,11 @@
+#
+#
+# Hello World in Many Programming Languages!!!
+#
+# Authors:
+# @author Ruben Andre Barreiro
+#
+#
+        
+# Prints "Hello World!!!" to the terminal window
 print("Hello World!!!")
