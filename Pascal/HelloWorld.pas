b6f6abf02ff03973871cc4cd782921739abdaf37
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-12 09:43:44 +0100

    Hello World in Pascal
    Hello World in Pascal

---
+++ Pascal/HelloWorld.pas
@@ -0,0 +1,10 @@
+(* Hello World in Many Programming Languages!!! *)
+(* Authors: *)
+(* - Ruben Andre Barreiro *)
+
+program Hello;
+begin
+  (* Prints "Hello World!!!" to the terminal window *)
+  writeln ('Hello World!!!');
+  readln;
+end.
