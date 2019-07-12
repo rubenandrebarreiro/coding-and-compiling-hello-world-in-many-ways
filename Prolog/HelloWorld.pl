340cfee7dcbe65c14124076cc54154c80d477e71
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-13 00:04:43 +0100

    Hello World in Prolog
    Hello World in Prolog

---
+++ Prolog/HelloWorld.pl
@@ -0,0 +1,9 @@
+% Hello World in Many Programming Languages!!!
+% Authors:
+% @author Ruben Andre Barreiro       
+
+:- initialization hello_world, halt.
+
+hello_world :-
+    % Prints "Hello World!!!" to the terminal window
+    write('Hello World!!!'), nl.
