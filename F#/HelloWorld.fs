d708b0963c4a62492569b7f38812a356273a1926
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-13 11:48:07 +0100

    Hello World in F#
    Hello World in F#

---
+++ F#/HelloWorld.fs
@@ -0,0 +1,10 @@
+// Hello World in Many Programming Languages!!!
+// Authors:
+// - Ruben Andre Barreiro
+open System
+
+[<EntryPoint>]
+let main argv =
+    // Prints "Hello World!!!" to the terminal window
+    printfn "Hello World!!!"
+    0 // return an integer exit code
