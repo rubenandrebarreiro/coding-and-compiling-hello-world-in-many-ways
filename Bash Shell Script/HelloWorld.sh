ece4a70c67eec2e80e7c45b3dc18aa55eea3e7e1
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-07-13 08:38:17 +0100

    Hello World in Bash Shell Script
    Hello World in Bash Shell Script

--- Bash Shell Script/HelloWorld.sh
+++ Bash Shell Script/HelloWorld.sh
@@ -1,11 +1,7 @@
-#
-#
 # Hello World in Many Programming Languages!!!
 #
 # Authors:
 # @author Ruben Andre Barreiro
-#
-#
         
 # Prints "Hello World!!!" to the terminal window
 echo "Hello World!!!"
